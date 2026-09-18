7650bd61b836b2c33bf63dbb641ff0b0d85cbec3
Abdaleam <lalaabdalaem@gmail.com>
2026-09-18 13:42:03 +0300

    publish

---
+++ src/main/java/rainy/longer/LongerDataGenerator.java
@@ -0,0 +1,11 @@
+package rainy.longer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LongerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
