1d9ad6cf63037feb7fc51298e1b457c7b805ec61
Abdaleam <lalaabdalaem@gmail.com>
2026-09-21 09:37:48 +0300

    i found the mistake finnaly truley last v3

--- src/main/java/rainy/longer/datagen/LongerRecipeProvider.java
+++ src/main/java/rainy/longer/datagen/LongerRecipeProvider.java
@@ -196,7 +196,7 @@ public class LongerRecipeProvider extends FabricRecipeProvider {
                         .unlockedBy(getHasName(LongerItems.INFECTED_PLANKS), has(LongerItems.INFECTED_PLANKS))
                         .save(output, "longer:wet_planks_from_cleaning");
 
-                DryingRecipeBuilder.dryingRecipe(RecipeCategory.MISC, Ingredient.of(LongerItems.WET_PLANKS),
+                DryingRecipeBuilder.dryingRecipe(RecipeCategory.MISC, Ingredient.of(LongerItems.WASHED_COBBLEESTONE),
                                 LongerItems.DRIED_COBBLESTONE, 1)
                         .unlockedBy(getHasName(LongerItems.CRUSHED_COBBLESTONE), has(LongerItems.CRUSHED_COBBLESTONE))
                         .save(output, "longer:dried_cobblestone_from_drying");
